fd8f9d98d24016d1730b595bae871f6b574cdb1e
erphsa <evgeni.reingold@phsa.com>
2024-09-10 11:55:38 -0400

    adding DB

--- fcpbackend/fcpbackend/settings.py
+++ fcpbackend/fcpbackend/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'fcpbackend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "fcp_service",
+            "passfile": ".fcp_pgpass",
+        },
     }
-}
+}}
 
 
 # Password validation
